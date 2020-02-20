bb70c544f56bf3f66868f5ffdb112bd1c70a7978
Owen Brady <KMDV47@motorolasolutions.com>
2020-02-20 14:49:11 +0000

    Adding margins to signup and login forms.

--- pizza-shop-client/src/app/interceptors/auth.guard.ts
+++ pizza-shop-client/src/app/interceptors/auth.guard.ts
@@ -11,9 +11,8 @@ import {
 @Injectable({ providedIn: "root" })
 export class AuthGuard implements CanActivate {
   constructor(
-    private router: Router
-  ) // private authService: AuthenticationService
-  {}
+    private router: Router // private authService: AuthenticationService
+  ) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     // const currentUser = this.authService.currentUserValue;
