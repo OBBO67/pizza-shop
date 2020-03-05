5f6fe2e48b885225c0818b222365bf1038f490f4
Owen Brady <owenbrady1994@gmail.com>
2020-03-05 21:21:32 +0000

    Add alerts functionality.

--- pizza-shop-client/src/app/alert/alert.component.ts
+++ pizza-shop-client/src/app/alert/alert.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 
 /*
  * Alert component passes alert messages to the template whenever
  * a message is recieved from the alert service. It does this by
- * subscribing to the alerts service getMessage() method which
+ * subscribing to the alerts service getAlert() method which
  * returns an Observable.
  */
 
@@ -13,7 +14,30 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./alert.component.css"]
 })
 export class AlertComponent implements OnInit {
-  constructor() {}
+  private subscription: Subscription;
+  message: any;
 
-  ngOnInit(): void {}
+  constructor(private alertService: AlertService) {}
+
+  ngOnInit(): void {
+    // on init subscribe to the AlertService and
+    // when a message is recieved then display
+    // approriate alert type
+    this.subscription = this.alertService.getAlert().subscribe(message => {
+      switch (message && message.type) {
+        case "success":
+          message.cssClass = "alert alert-success";
+          break;
+        case "error":
+          message.cssClass = "alert alert-danger";
+          break;
+      }
+
+      this.message = message;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
